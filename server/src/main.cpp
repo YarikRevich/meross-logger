29b4205929c579c2f9a552c48a487f3ce1bed029
Yaroslav Svitlytskyi <yariksvitlitskiy81@gmail.com>
2025-05-18 16:00:08 +0200

    feature: added server

---
+++ server/src/main.cpp
@@ -0,0 +1,63 @@
+#include <WiFi.h>
+#include <HTTPClient.h>
+#include <ESPAsyncWebServer.h>
+
+const char* ssid = "503";
+const char* password = "503a503b";
+
+const char* influxHost = "http://192.168.0.102:8086";
+const char* influxToken = "2lGFfU5nOF74whOHM1KXRAo-QQ992L9CplVHA_HfXZVI2NsDzRg1y-LbvMSonWdCa9futTqEtht68Tt0kXPBpQ==";
+const char* influxOrg = "public";
+const char* influxBucket = "public";
+
+AsyncWebServer server(80);
+
+String getStatus() {
+  HTTPClient http;
+
+  http.begin(String(influxHost) + "/api/v2/query?org=" + influxOrg);
+  http.addHeader("Authorization", "Token " + String(influxToken));
+  http.addHeader("Content-Type", "application/vnd.flux");
+  http.addHeader("Accept", "application/csv");
+
+  String fluxQuery = R"(
+    from(bucket: "public")
+      |> range(start: -1h)
+      |> filter(fn: (r) => r["_measurement"] == "status")
+  )";
+
+  int httpResponseCode = http.POST(fluxQuery);
+  String payload;
+
+  if (httpResponseCode > 0) {
+    payload = http.getString();
+  } else {
+    payload = String(httpResponseCode);
+  }
+
+  http.end();
+
+  return payload;
+}
+
+void setup() {
+  Serial.begin(115200);
+
+  WiFi.begin(ssid, password);
+  while (WiFi.status() != WL_CONNECTED) {
+    delay(1000);
+    Serial.print(".");
+  }
+
+  Serial.println("Connected to WIFI!");
+
+  server.on("/status", HTTP_GET, [](AsyncWebServerRequest* request) {
+    request->send(200, "text/plain", getStatus());
+  });
+
+  server.begin();
+}
+
+void loop() {
+  // nothing here
+}
